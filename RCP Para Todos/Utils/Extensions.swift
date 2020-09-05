46abf7138934e7ab21a1d4a69f876ef76b46ddea
Juan Tocino <juan.tocino@artekium.com>
2020-09-05 18:26:37 -0300

    Ahora el header RCP Para Todos posee un borde que cambia de color según el modo. Se agrega la imagen mis cursos en el inicio del instructor, falta nuevo curso.

--- RCP Para Todos/Utils/Extensions.swift
+++ RCP Para Todos/Utils/Extensions.swift
@@ -45,3 +45,23 @@ extension UINavigationController {
   }
 
 }
+
+@IBDesignable class BorderView : UIView {
+    @IBInspectable var borderColor: UIColor = .clear {
+        didSet {
+        layer.borderColor = borderColor.cgColor
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+}
