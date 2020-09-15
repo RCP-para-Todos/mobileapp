543bb6902d19061b09d982670c99a20257725736
Joaquín Krasuk <joaquin.krasuk@albany.company>
2020-09-15 00:02:51 -0300

    Pantalla de estadisticas con iconos, formulas de calculo

--- app/src/main/java/com/jk/rcp/main/activities/practicante/ActividadActivity.java
+++ app/src/main/java/com/jk/rcp/main/activities/practicante/ActividadActivity.java
@@ -3,6 +3,7 @@ package com.jk.rcp.main.activities.practicante;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,6 +11,7 @@ import androidx.appcompat.widget.Toolbar;
 import com.jk.rcp.R;
 import com.jk.rcp.main.data.adapter.EventListAdapter;
 import com.jk.rcp.main.data.model.event.Event;
+import com.jk.rcp.main.data.model.instant.Instant;
 import com.jk.rcp.main.data.model.user.User;
 
 public class ActividadActivity extends AppCompatActivity {
@@ -23,7 +25,7 @@ public class ActividadActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_estadisticas);
+        setContentView(R.layout.activity_actividad);
         globalUser = (User) getApplicationContext();
         Log.d(TAG, globalUser.toString());
 
@@ -37,6 +39,30 @@ public class ActividadActivity extends AppCompatActivity {
         if (getIntent().getExtras() != null) {
             event = (Event) getIntent().getSerializableExtra("event");
             Log.d(TAG, event.toString());
+
+            TextView tiempoTotalManiobra = findViewById(R.id.valueTiempoTotalManiobra);
+            tiempoTotalManiobra.setText(String.valueOf(Instant.tiempoTotalManiobra(event.getInstants())));
+
+            TextView porcentajeSobrevida = findViewById(R.id.valuePorcentajeSobrevida);
+            porcentajeSobrevida.setText(String.valueOf(Instant.porcentajeTotalSobreVida(event.getInstants())));
+
+            TextView tiempoInactividad = findViewById(R.id.valueTiempoInactividad);
+            tiempoInactividad.setText(String.valueOf(Instant.tiempoTotalInactividad(event.getInstants())));
+
+            TextView porcentajeInsuflacionesCorrectas = findViewById(R.id.valuePorcentajeInsuflacionesCorrectas);
+            porcentajeInsuflacionesCorrectas.setText(String.valueOf(Instant.porcentajeInsuflacionesCorrectas(event.getInstants())));
+
+            TextView porcentajeCompresionesCorrectas = findViewById(R.id.valuePorcentajeCompresionesCorrectas);
+            porcentajeCompresionesCorrectas.setText(String.valueOf(Instant.porcentajeCompresionesCorrectas(event.getInstants())));
+
+            TextView cantInsIncorrectasMalaPosicion = findViewById(R.id.valueCantidadInsuflacionesIncorrectasPorMalaPosicion);
+            cantInsIncorrectasMalaPosicion.setText(String.valueOf(Instant.cantidadInsuflacionesCorrectasPosicionCabeza(event.getInstants())));
+
+            TextView fuerzaPromedioAplicada = findViewById(R.id.valueFuerzaPromedioAplicada);
+            fuerzaPromedioAplicada.setText(String.valueOf(Instant.fuerzaPromedioAplicada(event.getInstants())));
+
+            TextView calidadInsuflaciones = findViewById(R.id.valueCalidadInsuflasiones);
+            calidadInsuflaciones.setText(Instant.calidadInsuflaciones(event.getInstants()));
         }
     }
 
--- app/src/main/java/com/jk/rcp/main/data/model/instant/Instant.java
+++ app/src/main/java/com/jk/rcp/main/data/model/instant/Instant.java
@@ -4,6 +4,7 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Instant implements Serializable {
 
@@ -64,6 +65,91 @@ public class Instant implements Serializable {
 
     public void setPosicion(String posicion) {
         this.posicion = posicion;
+    }
+
+    // Funciones de calculo
+    public static int tiempoTotalManiobra(List<Instant> instantes) {
+        int tiempoTotal = 0;
+        if (instantes.size() > 0) {
+            tiempoTotal = instantes.size();
+        }
+
+        return tiempoTotal / 2;
+    }
+
+    /* El tiempo de inactividad es solo contar las nulas sumando al tiempo de inactividad.
+     O solo seria tiempo de actividad (lo contrario) las correctas ?
+     */
+    public static Double tiempoTotalInactividad(List<Instant> instantes) {
+        Double tiempoTotalInactividad = 0.0;
+
+        for (Instant instante : instantes
+        ) {
+            if (instante.getCompresion() != null && instante.getCompresion().equals("Nula") && instante.getInsuflacion().equals("Nula")) {
+                tiempoTotalInactividad = tiempoTotalInactividad + 0.5;
+            }
+        }
+
+        return tiempoTotalInactividad;
+    }
+
+    /* Empiezo con 100% de porcentaje de sobrevida.
+     Cada segundo me resta un 10%.
+     */
+    public static Double porcentajeTotalSobreVida(List<Instant> instantes) {
+        Double porcentajeTotalSobreVida = 100.0;
+        Double tiempoTotalInactividad = Instant.tiempoTotalInactividad(instantes);
+        return porcentajeTotalSobreVida - tiempoTotalInactividad * 10;
+    }
+
+    public static Double porcentajeInsuflacionesCorrectas(List<Instant> instantes) {
+        Double insuflacionesTotalesSinNulas = 0.0;
+        Double insuflacionesCorrectas = 0.0;
+
+        for (Instant instante : instantes) {
+            if (instante.getInsuflacion() != null && (instante.getInsuflacion().equals("Insuficiente") || instante.getInsuflacion().equals("Excesiva"))) {
+                insuflacionesTotalesSinNulas = insuflacionesTotalesSinNulas + 1;
+            }
+            if (instante.getInsuflacion() != null && instante.getInsuflacion().equals("Correcta")) {
+                insuflacionesTotalesSinNulas = insuflacionesTotalesSinNulas + 1;
+                insuflacionesCorrectas = insuflacionesCorrectas + 1;
+            }
+        }
+        if (insuflacionesCorrectas == 0.0 && insuflacionesTotalesSinNulas == 0.0) {
+            return 0.0;
+        }
+        return (insuflacionesCorrectas / insuflacionesTotalesSinNulas) * 100;
+    }
+
+    public static Double porcentajeCompresionesCorrectas(List<Instant> instantes) {
+        Double compresionesCorrectas = 0.0;
+
+        for (Instant instante : instantes) {
+            if (instante.getCompresion() != null && instante.getCompresion().equals("Correcta")) {
+                compresionesCorrectas = compresionesCorrectas + 1;
+            }
+        }
+
+        return (compresionesCorrectas / Double.valueOf(instantes.size())) * 100;
+    }
+
+    // TODO
+    public static Double cantidadInsuflacionesCorrectasPosicionCabeza(List<Instant> instantes) {
+        return 1.0;
+    }
+
+    // TODO
+    public static Double fuerzaPromedioAplicada(List<Instant> instantes) {
+        return 10.0;
+    }
+
+    public static String calidadInsuflaciones(List<Instant> instantes) {
+        Double porcentajeInsuflacionesCorrectas = Instant.porcentajeInsuflacionesCorrectas(instantes);
+        if (porcentajeInsuflacionesCorrectas > 0.5) {
+            return "Optima";
+        } else {
+            return "Mala";
+        }
     }
 
     @Override
