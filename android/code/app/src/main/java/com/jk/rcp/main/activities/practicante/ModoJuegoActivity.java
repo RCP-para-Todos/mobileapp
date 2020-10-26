ab9c95bf2f8849883c551b0c538a326b6c9719fe
Joaquín Krasuk <joaquin.krasuk@albany.company>
2020-10-25 21:33:53 -0300

    Nuevo BLE, scan

--- android/code/app/src/main/java/com/jk/rcp/main/activities/practicante/ModoJuegoActivity.java
+++ android/code/app/src/main/java/com/jk/rcp/main/activities/practicante/ModoJuegoActivity.java
@@ -1,27 +1,54 @@
 package com.jk.rcp.main.activities.practicante;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.jk.rcp.R;
 import com.jk.rcp.main.activities.practicante.juego.JuegoPaso1Activity;
 
+import java.util.ArrayList;
+
 public class ModoJuegoActivity extends AppCompatActivity {
     private static final String TAG = "ModoJuegoActivity";
-    private static final int REQUEST_ENABLE_BT = 200;
     private Button btnJugar;
-    private BluetoothAdapter mBluetoothAdapter;
     private Spinner spinner;
     private int tiempo;
+
+    private enum ScanState {NONE, LESCAN, DISCOVERY, DISCOVERY_FINISHED}
+
+    private ScanState scanState = ScanState.NONE;
+    private static final long LESCAN_PERIOD = 10000; // similar to bluetoothAdapter.startDiscovery
+    private Handler leScanStopHandler = new Handler();
+    private BluetoothAdapter.LeScanCallback leScanCallback;
+    private BroadcastReceiver discoveryBroadcastReceiver;
+    private IntentFilter discoveryIntentFilter;
+
+    private BluetoothAdapter bluetoothAdapter;
+    private ArrayList<BluetoothDevice> listItems = new ArrayList<>();
+
+    private BluetoothDevice ESP32;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +59,10 @@ public class ModoJuegoActivity extends AppCompatActivity {
 
         btnJugar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                stopScan();
                 Intent intent = new Intent(ModoJuegoActivity.this, JuegoPaso1Activity.class);
                 intent.putExtra("tiempo", tiempo);
+                intent.putExtra("device", ESP32.getAddress());
                 startActivity(intent);
             }
         });
@@ -66,16 +95,149 @@ public class ModoJuegoActivity extends AppCompatActivity {
             }
         });
 
-        beginBLE();
+        leScanCallback = (device, rssi, scanRecord) -> {
+            if (device != null) {
+                runOnUiThread(() -> {
+                    Log.d(TAG, "En el callbakc");
+                    updateScan(device);
+                });
+            }
+        };
+
+        discoveryBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(BluetoothDevice.ACTION_FOUND)) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    if (device.getType() != BluetoothDevice.DEVICE_TYPE_CLASSIC) {
+                        runOnUiThread(() -> updateScan(device));
+                    }
+                }
+                if (intent.getAction().equals((BluetoothAdapter.ACTION_DISCOVERY_FINISHED))) {
+                    scanState = ScanState.DISCOVERY_FINISHED; // don't cancel again
+                    stopScan();
+                }
+            }
+        };
+        discoveryIntentFilter = new IntentFilter();
+        discoveryIntentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        discoveryIntentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        if(getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH))
+            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        startScan();
     }
 
-    public void beginBLE() {
-        // BLE
-        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        mBluetoothAdapter = bluetoothManager.getAdapter();
-        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    @SuppressLint("StaticFieldLeak") // AsyncTask needs reference to this fragment
+    private void startScan() {
+        if (scanState != ScanState.NONE)
+            return;
+        scanState = ScanState.LESCAN;
+        Log.d(TAG, "AC!");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                scanState = ScanState.NONE;
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(R.string.location_permission_title);
+                builder.setMessage(R.string.location_permission_message);
+                builder.setPositiveButton(android.R.string.ok,
+                        (dialog, which) -> requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0));
+                builder.show();
+                return;
+            }
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            boolean locationEnabled = false;
+            try {
+                locationEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            } catch (Exception ignored) {
+            }
+            try {
+                locationEnabled |= locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            } catch (Exception ignored) {
+            }
+            if (!locationEnabled)
+                scanState = ScanState.DISCOVERY;
+            // Starting with Android 6.0 a bluetooth scan requires ACCESS_COARSE_LOCATION permission, but that's not all!
+            // LESCAN also needs enabled 'location services', whereas DISCOVERY works without.
+            // Most users think of GPS as 'location service', but it includes more, as we see here.
+            // Instead of asking the user to enable something they consider unrelated,
+            // we fall back to the older API that scans for bluetooth classic _and_ LE
+            // sometimes the older API returns less results or slower
         }
+        listItems.clear();
+
+        if (scanState == ScanState.LESCAN) {
+            Log.d(TAG, "ACA?");
+            leScanStopHandler.postDelayed(this::stopScan, LESCAN_PERIOD);
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void[] params) {
+                    bluetoothAdapter.startLeScan(null, leScanCallback);
+                    return null;
+                }
+            }.execute(); // start async to prevent blocking UI, because startLeScan sometimes take some seconds
+        } else {
+            bluetoothAdapter.startDiscovery();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // ignore requestCode as there is only one in this fragment
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            new Handler(Looper.getMainLooper()).postDelayed(this::startScan, 1); // run after onResume to avoid wrong empty-text
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(getText(R.string.location_denied_title));
+            builder.setMessage(getText(R.string.location_denied_message));
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.show();
+        }
+    }
+
+    private void updateScan(BluetoothDevice device) {
+        Log.d(TAG, device.getName());
+
+        if (scanState == ScanState.NONE)
+            return;
+        if (listItems.indexOf(device) < 0) {
+            listItems.add(device);
+            if (device.getName().equals("ESP32")) {
+                this.ESP32 = device;
+                Log.d(TAG, "Tengo al ESP32!");
+                stopScan();
+            }
+        }
+    }
+
+    private void stopScan() {
+        if (scanState == ScanState.NONE)
+            return;
+
+        switch (scanState) {
+            case LESCAN:
+                leScanStopHandler.removeCallbacks(this::stopScan);
+                bluetoothAdapter.stopLeScan(leScanCallback);
+                break;
+            case DISCOVERY:
+                bluetoothAdapter.cancelDiscovery();
+                break;
+            default:
+                // already canceled
+        }
+        scanState = ScanState.NONE;
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopScan();
+        unregisterReceiver(discoveryBroadcastReceiver);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(discoveryBroadcastReceiver, discoveryIntentFilter);
     }
 }
--- android/code/app/src/main/java/com/jk/rcp/main/activities/practicante/juego/JuegoPaso1Activity.java
+++ android/code/app/src/main/java/com/jk/rcp/main/activities/practicante/juego/JuegoPaso1Activity.java
@@ -1,25 +1,12 @@
 package com.jk.rcp.main.activities.practicante.juego;
 
-import android.Manifest;
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattDescriptor;
-import android.bluetooth.BluetoothGattService;
-import android.bluetooth.BluetoothManager;
-import android.bluetooth.BluetoothProfile;
-import android.bluetooth.le.BluetoothLeScanner;
-import android.bluetooth.le.ScanCallback;
-import android.bluetooth.le.ScanResult;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -30,7 +17,6 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -53,22 +39,15 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Timer;
-import java.util.TimerTask;
-import java.util.UUID;
 
 import okhttp3.ResponseBody;
 
 public class JuegoPaso1Activity extends AppCompatActivity {
     private static final String TAG = "JuegoPaso1Activity";
-    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
-    private static final int REQUEST_ENABLE_BT = 200;
-    public ToggleButton toggleButton;
     public TextView textView;
-    BluetoothGattCharacteristic mGattChar;
     Timer timer;
     ProgressDialog progress;
     private Button btnJugar;
-    private BluetoothAdapter mBluetoothAdapter;
     private BluetoothGatt mBluetoothGatt;
     private List<Instant> instantes;
     private int mediosSegundos = 0;
@@ -87,114 +66,6 @@ public class JuegoPaso1Activity extends AppCompatActivity {
     private ImageView viento4;
     private TextView puntajeTextView;
     private User globalUser;
-    public final BluetoothGattCallback mBluetoothGattCallback = new BluetoothGattCallback() {
-        @Override
-        public void onCharacteristicChanged(BluetoothGatt gatt,
-                                            BluetoothGattCharacteristic characteristic) {
-            byte[] data = characteristic.getValue();
-            String s = new String(data);
-            String converted = s.substring(0, 8);
-            String[] splitted = converted.split(";");
-            Log.d(TAG, "Recibo ESP32: " + converted);
-            tratamientoRecepcionBluetooth(splitted);
-            // mBluetoothGatt.disconnect();
-        }
-
-        @Override
-        public void onConnectionStateChange(BluetoothGatt gatt,
-                                            int status,
-                                            int newState) {
-            if (newState == BluetoothProfile.STATE_CONNECTED) {
-                mBluetoothGatt.discoverServices();
-            }
-        }
-
-        @Override
-        public void onServicesDiscovered(BluetoothGatt gatt,
-                                         int status) {
-            if (status == BluetoothGatt.GATT_SUCCESS) {
-                BluetoothGattService mGattService =
-                        mBluetoothGatt.getService(UUID.fromString("6E400001-B5A3-F393-E0A9-E50E24DCCA9E"));
-                if (mGattService != null) {
-
-                    Log.i("onServicesDiscovered",
-                            "Service characteristic UUID found: " + mGattService.getUuid().toString());
-
-                    mGattChar =
-                            mGattService.getCharacteristic(UUID.fromString("6E400003-B5A3-F393-E0A9-E50E24DCCA9E"));
-
-                    if (mGattChar != null) {
-
-                        if (gatt.setCharacteristicNotification(mGattChar, true) == true) {
-                            Log.d("gatt.setCharacteristicNotification", "SUCCESS!");
-                        } else {
-                            Log.d("gatt.setCharacteristicNotification", "FAILURE!");
-                        }
-                        BluetoothGattDescriptor descriptor = mGattChar.getDescriptors().get(0);
-                        if (0 != (mGattChar.getProperties() & BluetoothGattCharacteristic.PROPERTY_INDICATE)) {
-                            // It's an indicate characteristic
-                            Log.d("onServicesDiscovered", "Characteristic (" + mGattChar.getUuid() + ") is INDICATE");
-                            if (descriptor != null) {
-                                descriptor.setValue(BluetoothGattDescriptor.ENABLE_INDICATION_VALUE);
-                                gatt.writeDescriptor(descriptor);
-                            }
-                        } else {
-                            // It's a notify characteristic
-                            Log.d("onServicesDiscovered", "Characteristic (" + mGattChar.getUuid() + ") is NOTIFY");
-                            if (descriptor != null) {
-                                descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
-                                gatt.writeDescriptor(descriptor);
-                            }
-                        }
-                        Log.i("onServicesDiscovered",
-                                "characteristic UUID found: " + mGattChar.getUuid().toString());
-
-                        final Runnable modifyIcons = new Runnable() {
-                            public void run() {
-                                sistemaIconos();
-                            }
-                        };
-
-                        TimerTask task = new TimerTask() {
-                            public void run() {
-                                runOnUiThread(modifyIcons);
-                            }
-                        };
-
-                        //Set timer, read temperature every 2S
-                        timer = new Timer();
-                        timer.scheduleAtFixedRate(task, 5, 125);
-
-                    } else {
-                        Log.i("onServicesDiscovered",
-                                "characteristic not found for UUID: " + mGattChar.getUuid().toString());
-
-                    }
-
-                } else {
-                    Log.i("onServicesDiscovered",
-                            "Service characteristic not found for UUID: " + mGattService.getUuid().toString());
-
-                }
-
-                if (progress.isIndeterminate()) progress.dismiss();
-            }
-        }
-
-    };
-    private ScanCallback mLeScanCallback = new ScanCallback() {
-        @Override
-        public void onScanResult(int callbackType, ScanResult result) {
-            super.onScanResult(callbackType, result);
-
-            Log.i("onScanResult", result.getDevice().getAddress());
-            Log.i("onScanResult", result.getDevice().getName());
-
-            scanLeDevice(false);
-
-            mBluetoothGatt = result.getDevice().connectGatt(getApplicationContext(), false, mBluetoothGattCallback);
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -220,28 +91,13 @@ public class JuegoPaso1Activity extends AppCompatActivity {
 
         instantes = new ArrayList<Instant>();
 
-        // toggleButton.setEnabled(false);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // Android M Permission check 
-            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("This app needs location access");
-                builder.setMessage("Please grant location access so this app can detect beacons.");
-                builder.setPositiveButton(android.R.string.ok, null);
-                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                    @Override
-                    public void onDismiss(DialogInterface dialog) {
-                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
-                    }
-                });
-                builder.show();
-                return;
-            }
-        }
-        beginBLE();
-
         if (getIntent().getExtras() != null && getIntent().getSerializableExtra("tiempo") != null) {
             int tiempo = (int) getIntent().getSerializableExtra("tiempo");
+            this.count = tiempo;
+        }
+
+        if (getIntent().getExtras() != null && getIntent().getSerializableExtra("device") != null) {
+            int tiempo = (int) getIntent().getSerializableExtra("device");
             this.count = tiempo;
         }
     }
@@ -415,34 +271,11 @@ public class JuegoPaso1Activity extends AppCompatActivity {
                 });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-    }
-
-    public void beginBLE() {
-        // BLE
-        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        mBluetoothAdapter = bluetoothManager.getAdapter();
-        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        } else {
-            scanLeDevice(true);
-        }
-    }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        if (mBluetoothAdapter != null && mBluetoothAdapter.isEnabled()) {
-            scanLeDevice(false);
-        }
-        if (mBluetoothGatt != null) {
-            mBluetoothGatt.close();
-        }
         if (timer != null) timer.cancel();
     }
 
@@ -455,65 +288,6 @@ public class JuegoPaso1Activity extends AppCompatActivity {
         }
         mBluetoothGatt.close();
         mBluetoothGatt = null;
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // User chose not to enable Bluetooth.
-        Log.i("onActivityResult", "requestCode = " + requestCode);
-        Log.i("onActivityResult", "resultCode = " + resultCode);
-        if (requestCode == REQUEST_ENABLE_BT) {
-            if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-                finish();
-                return;
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String[] permissions,
-                                           int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_REQUEST_COARSE_LOCATION: {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d("onRequestPermis...", "coarse location permission granted");
-                    beginBLE();
-                } else {
-                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setTitle("Functionality limited");
-                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
-                    builder.setPositiveButton(android.R.string.ok, null);
-                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                        @Override
-                        public void onDismiss(DialogInterface dialog) {
-                        }
-                    });
-                    builder.show();
-                }
-                return;
-            }
-        }
-    }
-
-    private void scanLeDevice(final boolean enable) {
-        final BluetoothLeScanner bluetoothLeScanner =
-                mBluetoothAdapter.getBluetoothLeScanner();
-
-        if (enable) {
-            progress = ProgressDialog.show(this, "Escaneando", "Aguarde un momento por favor");
-
-            bluetoothLeScanner.startScan(mLeScanCallback);
-            Log.i("scanLeDevice", "Start scan");
-
-        } else {
-            // if (progress.isIndeterminate()) progress.dismiss();
-
-            bluetoothLeScanner.stopScan(mLeScanCallback);
-            Log.i("scanLeDevice", "Stop scan");
-        }
     }
 
     private void tratamientoRecepcionBluetooth(String[] datosCorrectos) {
